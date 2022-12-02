1cff5f7725ce3d7af847aee2145d50d83c7b44cc
Aiden Vigue <aiden@vigue.me>
2022-12-01 19:57:41 -0500

    oop

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { default: axios } = require("axios");
 const FormData = require('form-data');
 const fs = require('fs');
 const { exit } = require("process");
+const forge = require("node-forge");
 
 const serverID = process.env.PLEX_SERVER_ID;
 const token = process.env.PLEX_SERVER_TOKEN;
-const endDir = "/config";
-var commonName = "";
+const endDir = "./";
 
 const plexHeaders = {
     "Accept": "*\/*",
@@ -18,77 +18,56 @@ const plexHeaders = {
     "user-agent": "PlexMediaServer/1.25.2.5319-c43dc0277"
 }
 
+console.log('Generating 2048-bit key-pair...');
+let keys = forge.pki.rsa.generateKeyPair(2048);
+console.log('Key-pair created.');
+
 axios.get(`https://plex.tv/api/v2/devices/${serverID}/certificate/subject`, {
     headers: plexHeaders
 }).then((resp) => {
-    commonName = resp.data.split("commonName=\"")[1].split("\"")[0];
+    let commonName = resp.data.split("commonName=\"")[1].split("\"")[0];
+    console.log(`Required common name: ${commonName}`);
 
-    const spawn = require('child_process').spawn;
-    const child = spawn('/usr/bin/openssl', ['req','-nodes','-newkey','rsa:2048','-keyout',`${endDir}/privkey.pem`,'-out',`${endDir}/req.csr`]);
+    console.log('Creating CSR ...');
+    let csr = forge.pki.createCertificationRequest();
+    csr.publicKey = keys.publicKey;
 
-    child.stderr.on('data', (data) => {
-        //console.log(data.toString());
-        if(data.indexOf("Country Name") != -1) {
-            child.stdin.write("\n");
-        }
-        if(data.indexOf("State or Province Name") != -1) {
-            child.stdin.write("\n");
-        }
-        if(data.indexOf("Locality Name") != -1) {
-            child.stdin.write("\n");
-        }
-        if(data.indexOf("Common Name") != -1) {
-            child.stdin.write(`${commonName}\n`);
-        }
-        if(data.indexOf("Organizational Unit Name") != -1) {
-            child.stdin.write(`\n`);
-        }
-        if(data.indexOf("Organization Name") != -1) {
-            child.stdin.write(`\n`);
-        }
-        if(data.indexOf("Email Address") != -1) {
-            child.stdin.write(`\n`);
-        }
-        if(data.indexOf("A challenge password") != -1) {
-            child.stdin.write(`\n`);
-        }
-        if(data.indexOf("An optional company name") != -1) {
-            child.stdin.write(`\n`);
-        }
-    });
+    csr.setSubject([{
+        name: 'commonName',
+        value: commonName
+    }]);
 
-    let plexHeadersCopy = plexHeaders;
+    csr.sign(keys.privateKey);
+    console.log('CSR signed');
 
-    child.on('close', (code) => {
-        console.log(`CSR generated for ${commonName}: exit code ${code}`);
-        const formData = new FormData();
-        formData.append('file', fs.createReadStream(`${endDir}/req.csr`));
-        fs.unlinkSync(`${endDir}/req.csr`);
-        axios.put(`https://plex.tv/api/v2/devices/${serverID}/certificate/csr?reason=missing&invalidIn=0`, formData, {
-            headers: Object.assign(plexHeadersCopy, formData.getHeaders())
-        }).then((csrPostResp) => {
-            if(csrPostResp.status == 204) {
-                console.log(`CSR uploaded for ${commonName}.`);
+    const formData = new FormData();
+    formData.append('file', forge.pki.certificationRequestToPem(csr));
 
-                //do loop waiting for cert
-                setTimeout(() => {
-                    const dlInterval = setInterval(() => {
-                        axios.get(`https://plex.tv/api/v2/devices/${serverID}/certificate/download`, {
-                            headers: plexHeaders
-                        }).then((certDownloadResp) => {
-                            if(certDownloadResp.status == 200) {
-                                clearInterval(dlInterval);
-                                fs.writeFileSync(`${endDir}/fullchain.pem`, certDownloadResp.data.toString());
-                                console.log(`Certificate downloaded`);
-                            } else {
-                                console.log(`Certificate for ${commonName} not ready yet (${certDownloadResp.status})`);
-                            }
-                        })
-                    }, 3000);
-                }, 15000);
-            }
-        }).catch((err) => {
-            console.log("Error uploading cert", err);
-        })
-    });
+    axios.put(`https://plex.tv/api/v2/devices/${serverID}/certificate/csr?reason=missing&invalidIn=0`, formData, {
+        headers: { ...plexHeaders, ...formData.getHeaders()}
+    }).then((csrPostResp) => {
+        if(csrPostResp.status == 204) {
+            console.log(`CSR uploaded for ${commonName}.`);
+
+            //do loop waiting for cert
+            const dlInterval = setInterval(() => {
+                axios.get(`https://plex.tv/api/v2/devices/${serverID}/certificate/download`, {
+                    headers: plexHeaders
+                }).then((certDownloadResp) => {
+                    if(certDownloadResp.status == 200) {
+                        clearInterval(dlInterval);
+                        fs.writeFileSync(`${endDir}/fullchain.pem`, certDownloadResp.data.toString());
+                        fs.writeFileSync(`${endDir}/privkey.pem`, forge.pki.privateKeyToPem(keys.privateKey));
+                        console.log(`Certificate downloaded`);
+                    } else {
+                        console.log(`Certificate not ready yet (${certDownloadResp.status})`);
+                    }
+                }).catch((err) => {
+                    console.log(`Download returned error ${err.status}`);
+                })
+            }, 3000);
+        }
+    }).catch((err) => {
+        console.log("Error uploading cert", err);
+    })
 })
